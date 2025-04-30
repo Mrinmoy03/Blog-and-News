13564a0b97eb64a81eea4bb8400b9b3d7226d312
Mrinmoy Bhattacharjee <98102401+Mrinmoy03@users.noreply.github.com>
2025-04-30 12:52:29 +0530

    Add files via upload

---
+++ src/Pages/Blogs.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+import "../styles/Blogs.css";
+import userImg from '../assets/images/user.jpg'
+const Blogs = ({onBack}) => {
+  const [showForm, setShowForm] = useState(false)
+  return (
+    <div className='blogs'>
+        <div className="blogs-left">
+            <img src={userImg} alt="user iamge" />
+        </div>
+        <div className="blogs-right">
+          {showForm ? (
+             <div className="blogs-right-form">
+             <h1>New Post</h1>
+             <form >
+               <div className="img-upload">
+                 <label htmlFor="file-upload" className="file-upload">
+                   <i className="bx bx-upload"></i> Upload Image
+                 </label>
+                 <input type="file" id='file-upload'/>
+               </div>
+               <input type="text" placeholder='Add Title (Max 60 Characters)' className='title-input' />
+               <textarea className='text-input'
+               placeholder='Add Text'></textarea>
+               <button type='submit' className='submit-btn'>Submit</button>
+             </form>
+            </div>
+          ) : (<button className="post-btn" onClick={()=> setShowForm(true)}>
+          Create New Post
+         </button>) }
+             
+            
+             <button className="blogs-close-btn" onClick={onBack}>
+               back <i className="bx bx-chevron-right"></i>
+             </button>
+        </div>
+    </div>
+  )
+}
+
+export default Blogs
+
+
+
