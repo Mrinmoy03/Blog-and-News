eea6f5a5c7a97948fc78c4ff464ced3eeebf11c0
Mrinmoy Bhattacharjee <98102401+Mrinmoy03@users.noreply.github.com>
2025-04-30 13:33:23 +0530

    Add files via upload

--- src/Pages/News.jsx
+++ src/Pages/News.jsx
@@ -3,6 +3,10 @@ import Weather from "../components/Weather";
 import Calender from "../components/Calender";
 import "../styles/news.css";
 import userImg from "../assets/images/user.jpg";
+import blogImg1 from '../assets/images/blog1.jpg'
+import blogImg2 from '../assets/images/blog2.jpg'
+import blogImg3 from '../assets/images/blog3.jpg'
+import blogImg4 from '../assets/images/blog4.jpg'
 import noImg from "../assets/images/no-img.png";
 import axios from "axios";
 import NewsModal from "../components/NewsModal";
@@ -143,7 +147,49 @@ const News = ({onShowBlogs}) => {
           onDeleteBookmark={handleBookmarkClick}
         />
 
-        <div className="my-blogs">My Blogs</div>
+        <div className="my-blogs">
+          <h1 className="my-blogs-heading">My Blogs</h1>
+          <div className="blog-posts">
+            <div className="blog-post">
+              <img src={blogImg1} alt="Post Image" />
+              <h3>Lorem ipsum dolor sit amet.</h3>
+              <div className="post-buttons">
+                <button className="editpost">
+                  <i className="bx bxs-edit"></i>
+                </button>
+                <button className="delete-post">
+                <i className="bx bxs-x-circle"></i>
+                </button>
+              </div>
+            </div>
+            <div className="blog-post">
+              <img src={blogImg2} alt="Post Image" />
+              <h3>Lorem ipsum dolor sit amet.</h3>
+              <div className="post-buttons">
+                <button className="editpost">
+                  <i className="bx bxs-edit"></i>
+                </button>
+                <button className="delete-post">
+                <i className="bx bxs-x-circle"></i>
+                </button>
+              </div>
+            </div>
+            <div className="blog-post">
+              <img src={blogImg3} alt="Post Image" />
+              <h3>Lorem ipsum dolor sit amet.</h3>
+              <div className="post-buttons">
+                <button className="editpost">
+                  <i className="bx bxs-edit"></i>
+                </button>
+                <button className="delete-post">
+                <i className="bx bxs-x-circle"></i>
+                </button>
+              </div>
+            </div>
+           
+          </div>
+
+        </div>
 
         <div className="weather-calender">
           <Weather />
