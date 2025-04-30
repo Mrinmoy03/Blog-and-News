c7cf44e91f86a15814df5bf94f6d74dc3fe12bb2
Mrinmoy Bhattacharjee <98102401+Mrinmoy03@users.noreply.github.com>
2025-05-01 00:26:56 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,81 @@
-import React from 'react'
-import News from './Pages/News'
+import React, { useEffect, useState } from "react";
+import News from "./Pages/News";
+import Blogs from "./Pages/Blogs";
+import { use } from "react";
 
 const App = () => {
-  return (
-    <div className='container'>
-      <div className="news-blog-app">
-      <News/>
-      </div>
-     
-    </div>
-  )
-}
+  const [showNews, setShowNews] = useState(true);
+  const [showBlogs, setShowBlogs] = useState(false);
+  const [blogs, setBlogs] = useState([]);
+  const [selectedPost, setSelectedPost] = useState(null);
+  const [isEditing, setIsEditing] = useState(false);
 
-export default App
+  useEffect(() => {
+    const savedBlogs = JSON.parse(localStorage.getItem("blogs")) || [];
+    setBlogs(savedBlogs);
+  }, []);
+
+  const handleCreateBlog = (newBlog, isEdit) => {
+    setBlogs((prevBlogs) => {
+      const updatedBlogs = isEdit
+        ? prevBlogs.map((blog) => (blog === selectedPost ? newBlog : blog))
+        : [...prevBlogs, newBlog];
+      localStorage.setItem("blogs", JSON.stringify(updatedBlogs));
+      return updatedBlogs;
+    });
+    setIsEditing(false);
+    setSelectedPost(null);
+  };
+
+  const handleEditBlog = (blog) => {
+    setSelectedPost(blog);
+    setIsEditing(true);
+    setShowNews(false);
+    setShowBlogs(true);
+  };
+  
+  const handleDeleteBlog = (blogToDelete) =>{
+      setBlogs((prevBlogs) => {
+        const updatedBlogs = prevBlogs.filter((blog) => blog !== blogToDelete)
+        localStorage.setItem('blogs', JSON.stringify(updatedBlogs))
+        return updatedBlogs
+      })
+  }
+
+
+  const handleShowBlogs = () => {
+    setShowNews(false);
+    setShowBlogs(true);
+  };
+  const handleBackToNews = () => {
+    setShowNews(true);
+    setShowBlogs(false);
+    setIsEditing(false);
+    setSelectedPost(null);
+  };
+  return (
+    // <div className='container'>
+    <div className="news-blog-app">
+      {showNews && (
+        <News
+          onShowBlogs={handleShowBlogs}
+          blogs={blogs}
+          onEditBlog={handleEditBlog}
+          onDeleteBlog = {handleDeleteBlog}
+        />
+      )}
+      {showBlogs && (
+        <Blogs
+          onBack={handleBackToNews}
+          onCreateBlog={handleCreateBlog}
+          editPost={selectedPost}
+          isEditing = {isEditing}
+        />
+      )}
+    </div>
+
+    //  </div>
+  );
+};
+
+export default App;
