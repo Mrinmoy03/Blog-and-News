d6a5223576ee629640728e5bc56fee5b08539a74
Mrinmoy Bhattacharjee <98102401+Mrinmoy03@users.noreply.github.com>
2025-05-01 00:26:04 +0530

    Add files via upload

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -10,7 +10,7 @@ const Weather = () => {
 
   useEffect(() => {
     const fetchDefaultLocation = async () => {
-      const defaultLocation = 'Kolkata';
+      const defaultLocation = 'West Bengal';
       const url = `https://api.openweathermap.org/data/2.5/weather?q=${defaultLocation}&units=Metric&appid=6a6bee0675ed62f02a10903bcc80c0b9`;
       const response = await axios.get(url);
       setData(response.data);
